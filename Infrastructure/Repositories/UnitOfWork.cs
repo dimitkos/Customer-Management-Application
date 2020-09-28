18a3169163b9aa3dd55ba490c64e7fa47997c979
dimitkos <User1@DESKTOP-D6C1HHM>
2020-09-28 15:02:41 +0300

    Implemet unit of work

---
+++ Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Interfaces;
+using Infrastructure.Data;
+using System;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        private bool disposed;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Commit()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+
+            disposed = true;
+        }
+    }
+}
