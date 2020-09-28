dd10634d75285f8a3263979fe831ca6653cdc8ce
dimitkos <User1@DESKTOP-D6C1HHM>
2020-09-28 15:34:47 +0300

    Implement CustomerRepository

---
+++ Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class CustomerRepository : GenericRepository<Customer>, ICustomerRepository
+    {
+        private readonly DbSet<Customer> _customer;
+
+        public CustomerRepository(ApplicationDbContext context) : base(context)
+        {
+            _customer = context.Set<Customer>();
+        }
+    }
+}
